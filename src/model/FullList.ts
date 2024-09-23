3fb521bace4d100365e965191687910726fb1a98
Amar Abbas <amarabbas1414@gmail.com>
2024-09-23 22:06:58 +0500

    model code

---
+++ src/model/FullList.ts
@@ -0,0 +1,27 @@
+import ListItem from "./ListItem";
+
+interface List {
+    list : ListItem[],
+    load() : void,
+    save() : void,
+    clearList() : void,
+    addItem(itemobj:ListItem) : void,
+    removeItem(id : string) : void,
+}
+
+export default class FullList implements List {
+
+    static instance : FullList = new FullList();
+
+    private constructor(
+        private _list: ListItem[] = [] 
+    ) {}
+
+    get list() : ListItem[] {
+        return this._list
+    }
+
+    save() : void {
+        
+    }
+}
